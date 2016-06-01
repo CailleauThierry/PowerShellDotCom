437f394169e000c7310bbec2dfcba05dbc690db2
Thierry Cailleau <Thierry.Cailleau@evault.com>
2016-06-01 15:46:20 +0200

    First tested version Commit on 6/1/2016 3:45 PM

---
+++ Get-FatLogs.ps1
@@ -0,0 +1,26 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2016 v5.2.122
+	 Created on:   	5/30/2016 5:09 PM
+	 Created by:   	Powershell.com <yourpowertip@powershell.com>
+	 Organization: 	Private
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file. Does not work in Posh v2 64-bits
+#>
+$path = "$env:windir\logs\cbs\"
+
+$space = Get-ChildItem -Path $path -Filter cbspersist*.cab -File |
+Measure-Object -Property Length -Sum |
+Select-Object -Property Count, Sum
+
+'{0} backed up log files eat up {0:n1} MB' -f $space.Count, ($space.Sum/1MB)
+
+#>> Running (Get-FatLogs.ps1) Script...
+#>> Platform: V5 64Bit
+#2 backed up log files eat up 2.0 MB
+#
+#>> Execution time: 00:00:03
+#>> Script Ended
