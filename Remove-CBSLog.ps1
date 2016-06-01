062a3a23f26394aeb0d7981975a751bb43863156
Thierry <thierrycailleau@yahoo.fr>
2016-06-01 17:09:27 +0200

    nice cbs logs cleaner Commit 6/1/2016 5:08 PM

---
+++ Remove-CBSLog.ps1
@@ -0,0 +1,21 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	yourpowertip@powershell.com
+	 Created on:   	6/1/2016 5:03 PM
+	 Created by:   	CailleauThierry
+	 Organization: 	Private
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+#requires -Version 1 
+#Requires -RunAsAdministrator 
+# must run with admin privileges! 
+
+Stop-Service TrustedInstaller
+Remove-Item -Path C:\Windows\Logs\CBS\CBS.log
+Start-Service TrustedInstaller
+
+#To delete the log file, you just need to make sure that the trusted installer isn't accessing (and locking) the file.
