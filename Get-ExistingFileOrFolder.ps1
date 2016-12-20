0c82b5946a93e4705e5d8c11affad54b3d5ce5ea
CailleauThierry <thierrycailleau@yahoo.fr>
2016-12-20 17:37:35 +0100

    Get-ExistingFileOrFolder.ps1 first version
    Originally from yourpowertip@powershell.com

---
+++ Get-ExistingFileOrFolder.ps1
@@ -0,0 +1,37 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Posted by : 	Powershell.com <yourpowertip@powershell.com>
+	 Created on:   	12/20/2016 5:25 PM
+	 Created by:   	CailleauThierry
+	 Organization: 	Private
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+#Test for File or Folder
+#All Versions
+#Test-Path can check whether a file or folder exists. If you add -PathType and specify Leaf (for files) or Container (for folders), the result can be even more specific:
+$path = 'c:\windows'
+
+Test-Path -Path $path
+Test-Path -Path $path -PathType Leaf # since "c:\windows" is not a "file" it return false, even if there are files (and folders) in "c:\windows")
+Test-Path -Path $path -PathType Container
+#Tested with PS v5 and v2 64-bits
+<#>> Running (Get-ExistingFileOrFolder.ps1) Script...
+>> Platform: V5 64Bit
+True
+False
+True
+
+>> Execution time: < 1 second
+>> Script Ended#>
+<#>> Running (Get-ExistingFileOrFolder.ps1) Script...
+>> Platform: V2 64Bit
+True
+False
+True
+
+>> Execution time: 00:00:01
+>> Script Ended#>
